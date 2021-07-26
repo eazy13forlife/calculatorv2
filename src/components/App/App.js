65e2034177fea1852ff9da3378ab50bea6cc876c
Eric B <eric_baffour-addo@alumni.brown.edu>
2021-07-26 02:42:38 -0700

    finished calculator funcitonality. Need to work on decimals tho

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Key from "../Key/Key.js";
 import "./App.scss";
 import keypad from "../../keypad";
+import {
+  onEqualsClick,
+  onOperatorClick,
+  onNumberClick,
+  reset,
+  deleteNumber,
+  onDecimalClick,
+} from "../../calculatorFuncs";
 
 const App = () => {
+  const [firstNumber, setFirstNumber] = useState("");
+  const [operator, setOperator] = useState(null);
+  const [secondNumber, setSecondNumber] = useState("");
+  const [view, setView] = useState(""); //what we see on calculator screen
+  const [equalsPressed, setEqualsPressed] = useState(false); //if equals button has been pressed
+
+  console.log(firstNumber, operator, secondNumber);
+
+  const onEqualButtonClick = () => {
+    return onEqualsClick(
+      firstNumber,
+      operator,
+      secondNumber,
+      setFirstNumber,
+      setOperator,
+      setSecondNumber,
+      setView,
+      setEqualsPressed,
+      equalsPressed
+    );
+  };
+
+  const onOperatorButtonClick = (operatorClicked) => {
+    return onOperatorClick(
+      firstNumber,
+      operator,
+      secondNumber,
+      setFirstNumber,
+      setOperator,
+      setSecondNumber,
+      setView,
+      operatorClicked,
+      setEqualsPressed,
+      equalsPressed
+    );
+  };
+
+  const onNumberButtonClick = (currentNumber) => {
+    return onNumberClick(
+      currentNumber,
+      firstNumber,
+      operator,
+      secondNumber,
+      setFirstNumber,
+      setSecondNumber,
+      setView,
+      setEqualsPressed,
+      equalsPressed
+    );
+  };
+
+  const onDecimalButtonClick = () => {
+    return onDecimalClick(
+      firstNumber,
+      operator,
+      secondNumber,
+      setFirstNumber,
+      setSecondNumber,
+      setView,
+      equalsPressed,
+      setEqualsPressed
+    );
+  };
+
+  const onResetButtonClick = () => {
+    reset(setFirstNumber, setOperator, setSecondNumber);
+  };
+
+  const onDeleteButtonClick = () => {
+    deleteNumber(
+      firstNumber,
+      secondNumber,
+      operator,
+      setOperator,
+      setFirstNumber,
+      setSecondNumber,
+      setView
+    );
+  };
   const renderedKeypad = keypad.map((keyValue, index) => {
-    console.log(keyValue);
     return (
       <React.Fragment key={index}>
-        <Key keyValue={keyValue} />
+        <Key
+          keyValue={keyValue}
+          onEqualButtonClick={onEqualButtonClick}
+          onOperatorButtonClick={onOperatorButtonClick}
+          onNumberButtonClick={onNumberButtonClick}
+          onResetButtonClick={onResetButtonClick}
+          onDeleteButtonClick={onDeleteButtonClick}
+          onDecimalButtonClick={onDecimalButtonClick}
+        />
       </React.Fragment>
     );
   });
@@ -17,7 +111,7 @@ const App = () => {
   return (
     <div className="Calculator">
       <div className="Calculator__screen Calculator__screen--theme">
-        <p className="Caluclator__result">20700</p>
+        <p className="Caluclator__result">{view}</p>
       </div>
       <div className="Calculator__body Calculator__body--theme">
         {renderedKeypad}
--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -2,7 +2,17 @@ import React from "react";
 
 import "./Key.scss";
 
-const Key = ({ keyValue, theme, additionalClass }) => {
+const Key = ({
+  keyValue,
+  theme,
+  additionalClass,
+  onEqualButtonClick,
+  onOperatorButtonClick,
+  onNumberButtonClick,
+  onDeleteButtonClick,
+  onResetButtonClick,
+  onDecimalButtonClick,
+}) => {
   const getKeyValue = () => {
     switch (keyValue) {
       case "+":
@@ -31,8 +41,33 @@ const Key = ({ keyValue, theme, additionalClass }) => {
       };
   };
 
+  const getOnClickValue = () => {
+    //if its a number or a decimal, we will run the onNumberButtonClick function
+    if (!isNaN(Number(keyValue))) {
+      return onNumberButtonClick(keyValue);
+    }
+    if (keyValue === ".") {
+      return onDecimalButtonClick();
+    }
+    if (keyValue === "=") {
+      return onEqualButtonClick();
+    }
+    if (keyValue === "del") {
+      return onDeleteButtonClick();
+    }
+    if (keyValue === "reset") {
+      return onResetButtonClick();
+    }
+    if (typeof keyValue === "string") {
+      return onOperatorButtonClick(keyValue);
+    }
+  };
+
   return (
-    <button className={`Key__button Key__button--${getKeyValue()}`}>
+    <button
+      className={`Key__button Key__button--${getKeyValue()}`}
+      onClick={getOnClickValue}
+    >
       <div
         className={`Key__value Key__value--${theme} ${getAdditionalClass()}`}
       >
